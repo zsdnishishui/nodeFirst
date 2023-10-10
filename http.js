81b4c472d44a09f5938a122e378ca9e41a699096
周圣道 <zsdnishishui@163.com>
2023-10-10 15:18:42 +0800

    把毒鸡汤写入文件

--- http.js
+++ http.js
@@ -1,6 +1,8 @@
 // 爬取毒鸡汤
 var myRequest = require('request')
 var myIconv = require('iconv-lite')
+var fs = require('fs');
+var readLine = require("readline");
 require('date-utils');
 var myEncoding = "utf-8";
 var seedURL = 'https://www.iamwawa.cn/home/dujitang/ajax';
@@ -15,13 +17,54 @@ function request(url, callback) {
     }
     myRequest(options, callback)
 }
+/**
+ * 按行读取文件内容
+ *
+ * @param fileName 文件名路径
+ * @param callback 回调函数
+ *
+ * @return 字符串数组
+ */
+function readFileToArr(fileName, callback) {
 
-var intervalId = setInterval(function () {
+    var arr = [];
+    var readObj = readLine.createInterface({
+        input: fs.createReadStream(fileName)
+    });
+
+    // 一行一行地读取文件
+    readObj.on('line', function (line) {
+        arr.push(line);
+    });
+    // 读取完成后,将arr作为参数传给回调函数
+    readObj.on('close', function () {
+        callback(arr);
+    });
+}
+console.log("开启爬虫")
+readFileToArr('test.txt', function (array) {
+    var intervalId = setInterval(function () {
     request(seedURL, function (err, res, body) {
         var html = myIconv.decode(body, myEncoding);
         var json = JSON.parse(html);
-        console.log(json.data);
+        var jitang = json.data;
+        // 去重
+        if (array.indexOf(jitang) === -1) {
+            var data = new Buffer(jitang+'\n')
+            // 追加文件
+            fs.appendFile('test.txt', data, (err) => {
+
+                // 追加失败
+                if(err) throw err
+
+            })
+        }
+
     })
-    // clearInterval(intervalId);	//清除定时器
-}, 5000);
+}, 3000);
+
+})
+
+
+
 
