b6c06d8b0be6479d52d9847d3a9019bf8987f69d
周圣道 <zsdnishishui@163.com>
2023-10-12 14:36:52 +0800

    添加注释

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const start = require('./http.js');
 const express = require('express')
 const app = express()
+// 使用解析post请求中的表单数据的中间件
 app.use(express.json())
-const PORT = process.env.PORT || 3000
-app.listen(PORT)
-console.log("started port:" + PORT)
-start()
+const PORT = 3000
+app.listen(PORT,function (){
+    console.log("started port:" + PORT)
+    start()
+})
